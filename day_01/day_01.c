f20da1c74595f09f71f09ba153fa7f856f6c674f
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-05-11 20:02:10 -0400

    update day 1

--- day_01/day_01.c
+++ day_01/day_01.c
@@ -1,79 +1,88 @@
+#include <assert.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int day1PartA(char filename[], int numOfExpenses) {
-    FILE* inputFile;
-    inputFile = fopen(filename, "r");
-    if(inputFile == NULL) {
-        perror("Failed to open file: ");
-        return 1;
-    }
+  FILE *inputFile;
+  inputFile = fopen(filename, "r");
+  if (inputFile == NULL) {
+    perror("Failed to open file");
+    return 1;
+  }
 
-    int expenses[numOfExpenses];
+  int expenses[numOfExpenses];
 
-    int i = 0;
-    char buffer[6];
-    while(fgets(buffer, sizeof(buffer) + 1, inputFile) && i < numOfExpenses) {
-        expenses[i++] = strtol(buffer, NULL, 10);
-    }
+  int i = 0;
+  char buffer[6];
+  while (fgets(buffer, sizeof(buffer) + 1, inputFile) && i < numOfExpenses) {
+    expenses[i++] = strtol(buffer, NULL, 10);
+  }
 
-    if(fclose(inputFile)) {
-        perror("Failed to close file: ");
-        return 1;
-    }
+  if (fclose(inputFile)) {
+    perror("Failed to close file");
+    return 1;
+  }
 
-    for(i = 0; i < numOfExpenses; i++) {
-        for(int j = i + 1; j < numOfExpenses; j++) {
-            if(expenses[i] + expenses[j] == 2020) {
-                printf("Day 1 Part A (%s):\t%d\n", filename, expenses[i] * expenses[j]);
-                return 0;
-            }
-        }
+  for (i = 0; i < numOfExpenses; i++) {
+    for (int j = i + 1; j < numOfExpenses; j++) {
+      if (expenses[i] + expenses[j] == 2020) {
+        return expenses[i] * expenses[j];
+      }
     }
-    return 0;
+  }
+  return 0;
 }
 
 int day1PartB(char filename[], int numOfExpenses) {
-    FILE* inputFile;
-    inputFile = fopen(filename, "r");
-    if(inputFile == NULL) {
-        perror("Failed to open file: ");
-        return 1;
-    }
+  FILE *inputFile;
+  inputFile = fopen(filename, "r");
+  if (inputFile == NULL) {
+    perror("Failed to open file");
+    return 1;
+  }
 
-    int expenses[numOfExpenses];
+  int expenses[numOfExpenses];
 
-    int i = 0;
-    char buffer[6];
-    while(fgets(buffer, sizeof(buffer) + 1, inputFile) && i < numOfExpenses) {
-        expenses[i++] = strtol(buffer, NULL, 10);
-    }
+  int i = 0;
+  char buffer[6];
+  while (fgets(buffer, sizeof(buffer) + 1, inputFile) && i < numOfExpenses) {
+    expenses[i++] = strtol(buffer, NULL, 10);
+  }
 
-    if(fclose(inputFile)) {
-        perror("Failed to close file: ");
-        return 1;
-    }
+  if (fclose(inputFile)) {
+    perror("Failed to close file");
+    return 1;
+  }
 
-    for(i = 0; i < numOfExpenses; i++) {
-        for(int j = i + 1; j < numOfExpenses; j++) {
-            for(int k = j + 1; k < numOfExpenses; k++) {
-                if(expenses[i] + expenses[j] + expenses[k] == 2020) {
-                    printf("Day 1 Part B (%s):\t%d\n", filename, expenses[i] * expenses[j] * expenses[k]);
-                    return 0;
-                }
-            }
+  for (i = 0; i < numOfExpenses; i++) {
+    for (int j = i + 1; j < numOfExpenses; j++) {
+      for (int k = j + 1; k < numOfExpenses; k++) {
+        if (expenses[i] + expenses[j] + expenses[k] == 2020) {
+          return expenses[i] * expenses[j] * expenses[k];
         }
+      }
     }
-    return 0;
+  }
+  return 0;
 }
 
 int main() {
-    day1PartA("example.txt", 6); // 514579
-    day1PartA("input.txt", 200); // 703131
+  int partA_example = day1PartA("example.txt", 6);
+  printf("Day 1 Part A (example):\t%d\n", partA_example);
+  assert(partA_example == 514579);
 
-    day1PartB("example.txt", 6); // 241861950
-    day1PartB("input.txt", 200); // 272423970
+  int partA_input = day1PartA("input.txt", 200);
+  printf("Day 1 Part A (input):\t%d\n", partA_input);
+  assert(partA_input == 703131);
 
-    return 0;
+  int partB_example = day1PartB("example.txt", 6);
+  printf("Day 1 Part B (example):\t%d\n", partB_example);
+  assert(partB_example == 241861950);
+
+  int partB_input = day1PartB("input.txt", 200);
+  printf("Day 1 Part B (input):\t%d\n", partB_input);
+  assert(partB_input == 272423970);
+
+  return 0;
 }
